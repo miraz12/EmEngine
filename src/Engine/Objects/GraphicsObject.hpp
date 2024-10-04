0a095a4ad498190fad885eda68300acec72e977a
Anton Christoffersson <anton_christoffersson@hotmail.com>
2024-10-04 22:40:59 +0200

    Skinning and animation

--- src/Engine/Objects/GraphicsObject.hpp
+++ src/Engine/Objects/GraphicsObject.hpp
@@ -1,6 +1,7 @@
 #ifndef GRAPHICSOBJECT_H_
 #define GRAPHICSOBJECT_H_
 
+#include "Rendering/Skin.hpp"
 #include <Camera.hpp>
 #include <Rendering/Animation.hpp>
 #include <Rendering/Material.hpp>
@@ -8,27 +9,43 @@
 #include <Rendering/Node.hpp>
 #include <Rendering/Primitive.hpp>
 #include <ShaderPrograms/ShaderProgram.hpp>
+#include <glm/gtc/quaternion.hpp> // For quaternion operations
+#include <glm/gtx/string_cast.hpp>
+#include <iostream>
 
 class GraphicsObject {
 public:
   GraphicsObject() = default;
   virtual ~GraphicsObject() = default;
 
+  // Add a new node to the object
   void newNode(glm::mat4 model);
-  virtual void draw(const ShaderProgram &sPrg);
-  virtual void drawGeom(const ShaderProgram &sPrg);
-  btCollisionShape *p_coll;
-  bool hasAnimation() { return p_numAnimations > 0; };
 
-  glm::mat4 p_modelMatrix = glm::identity<glm::mat4>();
-  std::vector<std::unique_ptr<Node>> p_nodes;
-  u32 p_numMats{0};
-  std::unique_ptr<Material[]> p_materials;
-  u32 p_numMeshes{0};
-  std::unique_ptr<Mesh[]> p_meshes;
-  u32 p_numAnimations{0};
-  std::unique_ptr<Animation[]> p_animations;
-  Material defaultMat;
+  // Virtual draw function using shader program
+  virtual void draw(const ShaderProgram &sPrg);
+
+  // Draw geometry using the shader program
+  virtual void drawGeom(const ShaderProgram &sPrg);
+
+  // Compute the local transformation matrix of the given node
+  glm::mat4 getLocalMat(i32 node);
+  // Compute the world matrix of a node by recursively combining with parent
+  // matrices
+  glm::mat4 getMatrix(i32 node);
+
+  // Bullet Physics collision shape
+  btCollisionShape *p_coll;
+
+  std::vector<std::unique_ptr<Node>> p_nodes; // Nodes of the object
+  u32 p_numMats{0};                           // Number of materials
+  std::unique_ptr<Material[]> p_materials;    // Array of materials
+  u32 p_numMeshes{0};                         // Number of meshes
+  std::unique_ptr<Mesh[]> p_meshes;           // Array of meshes
+  u32 p_numAnimations{0};                     // Number of animations
+  std::unique_ptr<Animation[]> p_animations;  // Array of animations
+  Material defaultMat;                        // Default material
+  u32 p_numSkins{0};                          // Number of skins
+  std::unique_ptr<Skin[]> p_skins;            // Array of skins
 };
 
 #endif // GRAPHICSOBJECT_H_
